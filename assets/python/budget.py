d51901739656876b05ea523bbf9f54579409e22f
Justin Yee <justin.yee529@gmail.com>
2020-06-21 17:19:40 -0700

    Adding css and javascript for asthetics including jquery

---
+++ assets/python/budget.py
@@ -0,0 +1,56 @@
+import sqlite3 as db
+class budgeter:
+    def __init__(self):
+        conn = db.connect('spend.db')
+        cur = conn.cursor()
+        sql = '''
+        create table if not exists expenses (
+            amount number,
+            category string, 
+            message string, 
+            date string
+            )
+        '''
+        cur.execute(sql)
+        conn.commit()
+
+    def log(self, amount, category, message=''):
+        from datetime import datetime
+        date = str(datetime.now())
+        conn = db.connect('spend.db')
+        cur = conn.cursor()
+        sql = '''
+        insert into expenses values (
+            {},
+            '{}',
+            '{}',
+            '{}'
+            )
+        '''.format(amount, category, message, date)
+        cur.execute(sql)
+        conn.commit()
+
+    def view(self, category=None):
+        conn = db.connect('spend.db')
+        cur = conn.cursor()
+        if category:
+            sql = '''
+            select * from expenses where category = '{}'
+            '''.format(category)
+            sql2 = '''select sum(amount) from expenses where category = '{}'
+            '''.format(category)
+        else:
+            sql = '''
+            select * from expenses
+            '''
+            sql2 = '''select sum(amount) from expenses
+            '''
+        cur.execute(sql)
+        result = cur.fetchall()
+        cur.execute(sql2)
+        total = cur.fetchone()[0]
+        return total, result
+    
+john = budgeter()
+john.log(100, 'movie', 'hello')
+print(john.view())
